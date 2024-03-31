c574e496f4c08315309b17dc78238e2417161937
stg <a.sergiotorres@gmail.com>
2024-03-31 13:21:58 +0200

    update: new path for pictures (to show in deployment)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -118,11 +118,11 @@ export const Home = () => {
                             description={item.description}
 
                             // there are only a few pics that repeat periodically
-                            imageUrl={`/Users/stg_1/studio_tattoo/src/img/s${item.id <= 4 ? item.id : item.id % 4}.png`}
+                            imageUrl={`/src/img/s${item.id <= 4 ? item.id : item.id % 4}.png`}
                         />
                     ))}
                 </div>
-                <img className="cover" src="/Users/stg_1/studio_tattoo/src/img/tattooCover.png" alt="Web cover picture. Heavily inked arm in blueish colors" />
+                <img className="cover" src="/img/tattooCover.png" alt="Web cover picture. Heavily inked arm in blueish colors" />
             </div>
         </>
     )
